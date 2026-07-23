99b3bd61e14bf7dcb64ce77a6e907e25c4a091d5
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-07-23 23:42:30 +0700

    refactor: update app theme to use primary color constant in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hydrosense_sn_app/core/constants/app_colors.dart';
 import 'package:hydrosense_sn_app/core/routes/app_router.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'HydroSense',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
